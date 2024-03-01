2d9d319fdd5e1da627cfbc06bda20971ca5c4554
notvenky <venkyp2000@gmail.com>
2024-03-01 18:44:18 -0500

    cleanup

--- basic_demo/hf_rep.py
+++ basic_demo/hf_rep.py
@@ -1,11 +1,3 @@
-"""
-This is a demo for using CogAgent and CogVLM in CLI
-Make sure you have installed vicuna-7b-v1.5 tokenizer model (https://huggingface.co/lmsys/vicuna-7b-v1.5), full checkpoint of vicuna-7b-v1.5 LLM is not required.
-In this demo, We us chat template, you can use others to replace such as 'vqa'.
-Strongly suggest to use GPU with bfloat16 support, otherwise, it will be slow.
-Mention that only one picture can be processed at one conversation, which means you can not replace or insert another picture during the conversation.
-"""
-
 import argparse
 import torch
 
@@ -13,11 +5,9 @@ from PIL import Image
 from transformers import AutoModelForCausalLM, LlamaTokenizer
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--quant", choices=[4], type=int, default=None, help='quantization bits')
 parser.add_argument("--from_pretrained", type=str, default="THUDM/cogagent-chat-hf", help='pretrained ckpt')
 parser.add_argument("--local_tokenizer", type=str, default="lmsys/vicuna-7b-v1.5", help='tokenizer path')
 parser.add_argument("--fp16", action="store_true")
-parser.add_argument("--bf16", action="store_true")
 
 args = parser.parse_args()
 MODEL_PATH = args.from_pretrained
@@ -25,29 +15,17 @@ TOKENIZER_PATH = args.local_tokenizer
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 tokenizer = LlamaTokenizer.from_pretrained(TOKENIZER_PATH)
-if args.bf16:
-    torch_type = torch.bfloat16
-else:
-    torch_type = torch.float16
+torch_type = torch.float16 # on A100, use torch.bfloat16
 
 print("========Use torch type as:{} with device:{}========\n\n".format(torch_type, DEVICE))
 
-if args.quant:
-    model = AutoModelForCausalLM.from_pretrained(
-        MODEL_PATH,
-        torch_dtype=torch_type,
-        low_cpu_mem_usage=True,
-        load_in_4bit=True,
-        trust_remote_code=True
-    ).eval()
-else:
-    model = AutoModelForCausalLM.from_pretrained(
-        MODEL_PATH,
-        torch_dtype=torch_type,
-        low_cpu_mem_usage=True,
-        load_in_4bit=args.quant is not None,
-        trust_remote_code=True
-    ).to(DEVICE).eval()
+model = AutoModelForCausalLM.from_pretrained(
+    MODEL_PATH,
+    torch_dtype=torch_type,
+    low_cpu_mem_usage=True,
+    load_in_4bit=args.quant is not None,
+    trust_remote_code=True
+).to(DEVICE).eval()
 
 text_only_template = "A chat between a curious user and an artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions. USER: {} ASSISTANT:"
 
